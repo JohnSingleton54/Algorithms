89dd26f2c2c82a00be4213bcf1b6c0a84be667db
John M. Singleton <37783430+JohnSingleton54@users.noreply.github.com>
2018-04-14 19:09:51 -0600

    Update file
    I found the bug in the constructFlowchart method.  I also wrote the code that reads the text from an input file and stores it in a ArrayList of Characters.

--- Exact String Matching/KMP.java
+++ Exact String Matching/KMP.java
@@ -1,5 +1,5 @@
 // John M. Singleton
-// started:  W 4/11/18, last revision:  R 4/12/18
+// started:  W 4/11/18, last revision:  Sa 4/14/18
 // CSCI 532
 // Project
 // the Knuth-Morris-Pratt (KMP) Algorithm
@@ -27,36 +27,33 @@ public class KMP {
 		}
 		
 		
-		// The following code is from another program, but it may help me input the text.
-/*		String fileName = "KMP_InputFile.txt";
-		int degree;
-		double [] poly1, poly2;
-		try { Scanner sc = new Scanner( new File( "HW1_InputFile.txt" ) ); 
-		degree = sc.nextInt();
-		poly1 = new double [ degree + 1 ];
-		poly2 = new double [ degree + 1 ];
-		for ( int i = 0; i <= degree; i++ ) {
-			poly1 [ degree - i ] = sc.nextDouble();
+		String fileName = "KMP_InputFile.txt";
+		// ArrayList - See pp. 229-232 of Lewis and Loftus 9e.  An ArrayList stores references to
+		// objects, not primitive values.  So I had to use the wrapper class Character instead of
+		// the primitive type char.
+		ArrayList<Character> text = new ArrayList<Character>();
+		try { Scanner sc = new Scanner( new File( fileName ) );
+		String characters = sc.next();
+		for( int i = 0; i < characters.length(); i++) {
+			text.add( i, characters.charAt( i ) );		
 		}
-		for ( int i = 0; i <= degree; i++ ) {
-			poly2 [ degree - i ] = sc.nextDouble();
+		int n = text.size();
+		for( int i = 0; i < n; i++ ) {
+			System.out.println( text.get( i ) );
 		}
-		System.out.println( "degree = " + degree );
-		System.out.println( Arrays.toString( poly1 ) );
-		System.out.println( Arrays.toString( poly2 ) );
 		}
-		catch( IOException e ) { System.err.println( e ); }*/
+		catch( IOException e ) { System.err.println( e ); }
 	}
-	
-	// The following method constructs the KMP flowchart.
-	// *** The following method has a bug.  It is not correctly constructing the flowchart for
-	// pattern ABABABCB.  (See Figure 11.6 on p. 492.)
+ 	
+	// The following method constructs the KMP flowchart.  (See Algorithm 11.2 on p. 493.)
+	// It is correctly constructing the flowchart for pattern ABABABCB.  (See Figure 11.6 on p.
+	// 492.)
 	public static void constructFlowchart( char [] pattern, int m, int [] fail ) {
 		int k, s;
 		fail[0] = -1;
 		for( k = 1; k < m; k++ ) {
 			s = fail[k-1];
-			while( s >= 1 ) {
+			while( s >= 0 ) {
 				if( pattern[s] == pattern[k-1] )
 				    break;
 				s = fail[s];
