ad80d07696448f546c6ca2ed325b95d582ebfa7b
John M. Singleton <37783430+JohnSingleton54@users.noreply.github.com>
2018-05-09 11:51:16 -0600

    Fixed bug in program
    Changing the "<" on line 120 to "<=" seemed to be the key in fixing a bug where not all of the patterns taken from the text were being found in the text.

--- Exact String Matching/BM.java
+++ Exact String Matching/BM.java
@@ -1,5 +1,5 @@
 // John M. Singleton
-// started:  Sa 4/21/18, last revision:  T 4/24/18
+// started:  Sa 4/21/18, last revision:  R 4/26/18
 // CSCI 532
 // Project
 // the Boyer-Moore (BM) Algorithm
@@ -18,32 +18,30 @@ public class BM {
 		System.out.println( (int)'Z' ); output 90
 		System.out.println( (char)65 ); output A*/
 		
-	
 		BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
 		System.out.print( "Enter a pattern to search for:  ");
 		String input_pattern = reader.readLine();
-		// The flowchart representation uses two arrays.  One contains the characters of the
-		// pattern (pattern), and the other contains the failure links (fail).
 		char [] pattern = input_pattern.toCharArray();
 		int m = pattern.length;
 		int [] charJump = new int [ alpha ];
 		
-		computeJumps( pattern, m, alpha, charJump );
+		int count4a = computeJumps( pattern, m, alpha, charJump );
 		
-		for( int i = 0; i < alpha; i++ ) {
+		/*for( int i = 0; i < alpha; i++ ) {
 		    System.out.print( charJump[i] + " ");
 		}
-		System.out.print( "\n" );
+		System.out.print( "\n" );*/
 		
 		int [] matchJump = new int [ m ];
 		
-		computeMatchJumps( pattern, m, matchJump );
-		for( int i = 0; i < m; i++ ) {
+		int count4b = computeMatchJumps( pattern, m, matchJump );
+		
+		/*for( int i = 0; i < m; i++ ) {
 		    System.out.print( matchJump[i] + " ");
 		}
-		System.out.print( "\n" );		
+		System.out.print( "\n" );*/		
 		
-		String fileName = "KMP_InputFile.txt";
+		String fileName = "InputFile.txt";
 		ArrayList<Character> text = new ArrayList<Character>();
 		try { Scanner sc = new Scanner( new File( fileName ) );
 		String characters = sc.next();
@@ -56,76 +54,116 @@ public class BM {
 			System.out.println( text.get( i ) );
 		}*/
 		
+		int [] match = boyerMooreScan( pattern, text, m, charJump, matchJump );
+		int count45 = count4a + count4b + match[1];
+		if( match[0] == -1 ) {
+			System.out.println( "No match was found.  count45:  " + count45 );
+		}
+		else {
+			System.out.println( "A match was found at index " + match[0] + ".  count45:  " + count45 );
+		}
 		
 		}
 		catch( IOException e ) { System.err.println( e ); }
 	}
 	
-	public static void computeJumps( char [] pattern, int m, int alpha, int [] charJump ) {
+	public static int computeJumps( char [] pattern, int m, int alpha, int [] charJump ) {
+		int count4a = 0;
 		for( int i = 0; i < alpha; i++) {
 			charJump[i] = m;
+			count4a++;
 		}
 		for( int k = 0; k < m; k++ ) {
-			charJump[ (int)pattern[k] - 65 ]= m - k - 1;
+			charJump[ (int)pattern[k] - 65 ] =  m - k - 1;
+			count4a++;
 		}
+		return count4a;
 	}
  
-	public static void computeMatchJumps( char [] pattern, int m, int [] matchJump ) {
-		 for( int k = 0; k < m; k++ ) {
-		 	matchJump[k] = m + 1;  // This represents an impossibly large slide.
-		 }
+	public static int computeMatchJumps( char [] pattern, int m, int [] matchJump ) {
+		int count4b = 0;
+		for( int k = 0; k < m; k++ ) {
+			matchJump[k] = m + 1;  // This represents an impossibly large slide.
+			count4b++;
+		}
 		 
-		 int [] suffix = new int [ m + 1 ];
-		 suffix[m] = m + 1;
-		 for( int k = m - 1; k >= 0; k-- ) {
+		int [] suffix = new int [ m + 1 ];
+	 	suffix[m] = m + 1;
+		for( int k = m - 1; k >= 0; k-- ) {
 		 	int s = suffix[k+1];
 		    //System.out.println( "in for loop" );			
 			while( s <= m ) {
 				//System.out.println( "in while loop" );
+				count4b++;
 				if( pattern[k] == pattern[s-1] ) {
 					break;
 				}
 				matchJump[s-1] = Math.min( matchJump[s-1], s - (k + 1) );
+				count4b++;
 				s = suffix[s];
 			}
 			suffix[k] = s - 1;
-		 }
-		 for( int i = 0; i < m + 1; i++) {
+		}
+	 	/*for( int i = 0; i < m + 1; i++) {
 		 	System.out.print( suffix[i] );
-		 }
-		 System.out.print( "\n" );
+		}
+		System.out.print( "\n" );
 		 
-		 System.out.println( "slide values prior to final while loop:" );
-		 for( int i = 0; i < m; i++) {
+		System.out.println( "slide values prior to final while loop:" );
+		for( int i = 0; i < m; i++) {
 		 	System.out.print( matchJump[i] );
-		 }
-		 System.out.print( "\n" );
-		 
-		 int low = 1;
-		 int shift = suffix[0];
-		 while( shift < m ) {
+		}
+		System.out.print( "\n" );*/
+	 
+		int low = 1;
+		int shift = suffix[0];
+		while( shift <= m ) {
 		 	for( int k = low; k <= shift; k++ ) {
-				if( k == 3) {
-					System.out.println( "k = 3:  matchJump[3] = " + matchJump[3] + ", shift = " + shift );
-				}
 				matchJump[k-1] = Math.min( matchJump[k-1], shift );
-			}
+				count4b++;
+            }
 			low = shift + 1;
 			shift = suffix[shift];
-			System.out.println( "shift:  " + shift );
-		 }
-		 System.out.println( "slide values:" );
-		 for( int i = 0; i < m; i++) {
+			//System.out.println( "shift:  " + shift );
+		} 
+		/*System.out.println( "slide values:" );
+		for( int i = 0; i < m; i++) {
 		 	System.out.print( matchJump[i] );
-		 }
-		 System.out.print( "\n" );
+		}
+		System.out.print( "\n" );*/
 		 
-		 for( int k = 0; k < m; k++) {
+		for( int k = 0; k < m; k++) {
 		 	matchJump[k] += (m - k - 1);
-		 }
-		 
-		 return;
+			count4b++;
+		}
+		return count4b;
 	}
- 
+	
+	public static int [] boyerMooreScan( char [] pattern, ArrayList<Character> text, int m, int [] charJump, int [] matchJump ) {
+	    int [] match = new int[2];
+		match[0] = -1;
+		int count5 = 0;
+		int j = m - 1;  // j indexes text characters
+		int k = m - 1;  // k indexes the pattern
+		while( j < text.size() ) {
+			if( k < 0 ) {
+				match[0] = j + 1;  // A match was found!
+				break;
+			}
+			count5++;
+			if( text.get( j ) == pattern[ k ] ) {
+				j--;
+				k--;
+			}
+			else {
+			    count5++;
+				j += Math.max( charJump[(int)text.get(j) - 65], matchJump[k] );
+				k = m - 1;
+			}
+		}
+		match[1] = count5;
+        return match;
+	}
+	
 }
 
